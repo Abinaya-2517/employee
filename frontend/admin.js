81763e1601027c9ee782c59da93ee9a22831fe94
Abinaya-2517 <kabinayakannan@gmail.com>
2024-02-28 08:21:38 +0530

    first commit

--- frontend/admin.js
+++ frontend/admin.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
       event.preventDefault(); 
       const emailid= document.getElementById('emailid').value;
       const password = document.getElementById('password').value;
-      fetch('http://localhost:3001/admin', {
+      fetch('https://emloyee-manangement-system.onrender.com/admin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
